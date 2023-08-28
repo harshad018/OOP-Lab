369e6bd779e2fcec003e7aedf95346b5cf6ea209
harshad018 <108193335+harshad018@users.noreply.github.com>
2023-08-28 11:10:52 +0530

    Add files via upload

---
+++ Singlearray.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+class Singlearray {
+
+    public static void main ( String args[]){
+
+	int rollno[] = new int[10];
+
+
+	for ( int i= 0; i < 10; i++){
+
+
+	    rollno[i] = i + 1;
+	    
+
+
+
+
+	}
+
+	for ( int j = 0; j < 10; j++){
+
+	    System.out.println(rollno[j]);
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
